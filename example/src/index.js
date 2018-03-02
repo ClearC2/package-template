617fd583f4426e0411a164b37b87205ac4889bb1
Nick Swope <nswope@clearc2.com>
2018-03-02 09:24:11 -0600

    husky test commit

--- example/src/index.js
+++ example/src/index.js
@@ -1,9 +1,6 @@
-import React from 'react'
 import ReactDOM from 'react-dom'
 
 const appElement = document.getElementById('app-root')
-
-console.log('linter test commit')
 
 ReactDOM.render(
   <h1>C2 Map</h1>,
